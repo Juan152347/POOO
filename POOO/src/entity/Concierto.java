e1762e77264840d6e05b2865bafeea59e753103f
PC <PC@LAPTOP-5M4O54BA>
2019-10-16 17:34:25 -0500

    Juan 1

--- POOO/src/entity/Concierto.java
+++ POOO/src/entity/Concierto.java
@@ -2,4 +2,10 @@ package entity;
 
 public class Concierto implements ServicioAdicional {
 
+	@Override
+	public double calcularPrecio() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
 }
--- POOO/src/enu/Enum.java
+++ POOO/src/enu/Enum.java
@@ -1,14 +1,7 @@
 package enu;
 
-<<<<<<< HEAD
-public class Enum {
 
-
-	
-	
-	
-=======
 public enum Enum {
 TAXI,MINIVAN,PARTILCULAR;
->>>>>>> branch 'master' of https://github.com/Juan152347/POOO.git
+
 }
