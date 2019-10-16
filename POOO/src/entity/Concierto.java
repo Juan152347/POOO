9f2a45aac51f2cc640de40b5bd6790577d8da3d6
Usuario <Usuario@10.154.21.236>
2019-10-16 17:24:43 -0500

    Mateo 1

--- POOO/src/entity/Concierto.java
+++ POOO/src/entity/Concierto.java
@@ -1,5 +1,40 @@
 package entity;
 
 public class Concierto implements ServicioAdicional {
+	private String artista;
+	private String lugar;
+	private String horaIngreso;
+	
+	public Concierto(String artista, String lugar, String horaIngreso) {
+		this.artista = artista;
+		this.lugar = lugar;
+		this.horaIngreso = horaIngreso;
+	}
 
+	public String getArtista() {
+		return artista;
+	}
+
+	public void setArtista(String artista) {
+		this.artista = artista;
+	}
+
+	public String getLugar() {
+		return lugar;
+	}
+
+	public void setLugar(String lugar) {
+		this.lugar = lugar;
+	}
+
+	public String getHoraIngreso() {
+		return horaIngreso;
+	}
+
+	public void setHoraIngreso(String horaIngreso) {
+		this.horaIngreso = horaIngreso;
+	}
+	
+	
+	
 }
--- POOO/src/entity/Transporte.java
+++ POOO/src/entity/Transporte.java
@@ -1,5 +1,39 @@
 package entity;
 
 public class Transporte implements ServicioAdicional{
+	private double distancia;
+	private TipoTransporte tipo;
+	private int numeroPasajeros;
+	
+	public double getDistancia() {
+		return distancia;
+	}
 
+	public void setDistancia(double distancia) {
+		this.distancia = distancia;
+	}
+	
+	public TipoTransporte getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(TipoTransporte tipo) {
+		this.tipo = tipo;
+	}
+
+	public int getNumeroPasajeros() {
+		return numeroPasajeros;
+	}
+
+	public void setNumeroPasajeros(int numeroPasajeros) {
+		this.numeroPasajeros = numeroPasajeros;
+	}
+
+	public Transporte(double distancia, TipoTransporte tipo, int numeroPasajeros) {
+		this.distancia = distancia;
+		this.tipo = tipo;
+		this.numeroPasajeros = numeroPasajeros;
+	}
+	
+	
 }
