15d662b10571c09e037fdace9e4ed245784d4a2b
PC <PC@LAPTOP-5M4O54BA>
2019-10-17 17:10:47 -0500

    juan

--- POOO/src/control/GestionCliente.java
+++ POOO/src/control/GestionCliente.java
@@ -5,8 +5,16 @@ import java.util.HashMap;
 import entity.Cliente;
 
 public class GestionCliente {
-	public void insertarCliente(Long cod,String nombre, String telefono, HashMap<Long,Cliente> listaclientes) {
-     Cliente ncli=new Cliente(nombre,telefono);
-     listaclientes.put(cod,ncli);
+	public void insertarCliente(Long cod, String nombre, String telefono, HashMap<Long, Cliente> listaclientes) {
+		Cliente ncli = new Cliente(nombre, telefono);
+		listaclientes.put(cod, ncli);
+	}
+
+	public boolean verificarexistencia(HashMap<Long, Cliente> listaclientes, long cod) {
+		if (listaclientes.get(cod) != null) {
+			return true;
+		} else {
+			return false;
+		}
 	}
 }
