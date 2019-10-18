94637a6890c9fa8dc8f2d79b9c413a3831eb3901
HP <HP@10.153.103.126>
2019-10-18 16:41:27 -0500

    ardila12

--- POOO/src/entity/Transporte.java
+++ POOO/src/entity/Transporte.java
@@ -44,23 +44,27 @@ public class Transporte extends ServicioAdicional {
 	@Override
 	public double calcularPrecio() {
 		int cantbuses = (int)Math.ceil(this.numeroPasajeros / this.tipo.getCanpasajero());
-		
 		double aumento;
 
+		
+		if(tipo==TipoTransporte.TAXI) {
+			precio = (distancia * precio);
+			precio*=cantbuses;
+		}
+		
 		if (tipo == TipoTransporte.MINIVAN) {
 
 			precio = (distancia * precio);
 			aumento = precio * 0.5;
 			precio += aumento;
-
+			precio*=cantbuses;
 		}
-		if (tipo == TipoTransporte.PARTILCULAR)
-
-		{
+		if (tipo == TipoTransporte.PARTILCULAR)	{
 
 			precio = (distancia * precio);
 			aumento = precio * 0.25;
 			precio += aumento;
+			precio*=cantbuses;
 
 		}
 
