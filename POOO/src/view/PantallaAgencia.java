7d3787f86bb4d2cb40d1d439d788ea7158a4b9fa
PC <PC@LAPTOP-5M4O54BA>
2019-10-20 12:35:07 -0500

    k

--- POOO/src/view/PantallaAgencia.java
+++ POOO/src/view/PantallaAgencia.java
@@ -3,6 +3,7 @@ package view;
 import control.ControlAgencia;
 import control.GestionCliente;
 import control.GestionTours;
+import enu.TipoEmpresa;
 
 import java.util.Calendar;
 import java.util.Date;
@@ -22,8 +23,7 @@ public class PantallaAgencia {
       gc.insertarCliente((long)98745634, "n4", "7777777", co.getListaClientes());
       gc.insertarCliente((long)98623148, "n5", "8888888", co.getListaClientes());
       co.verListacliente();
-      co.llenarTours();
-      co.verListatours();
+      
       
       Calendar c=Calendar.getInstance();
       c.set(Calendar.HOUR_OF_DAY, 0);
@@ -68,7 +68,13 @@ public class PantallaAgencia {
       Date fr9=c.getTime();
       gt.insertarTour((long)9865321, "nc1", "lp1", fr1, fs1, 1000, co.getListaTours());
       gt.insertarTour((long)7896325, "nc2", "lp2", fr2, fs2, 1000, co.getListaTours());
-      
+      gt.insertarTour((long)9856475, "nc3", "lp3", fr3, fs3, 1000, co.getListaTours());
+      gt.insertarTourEc((long)9632541, "nc4", "lp4", fr4, fs4, 2000, co.getListaTours(), true, 500, false);
+      gt.insertarTourEc((long)5863215, "nc5", "lp5", fr5, fs5, 2000, co.getListaTours(), true, 500, false);
+      gt.insertarTourEc((long)9462138, "nc6", "lp6", fr6, fs6, 2000, co.getListaTours(), true, 500, false);
+      gt.insertarTourEm((long)5462318, "nc7", "lp7", fr7, fs7, 3000, co.getListaTours(), "ne1", false, TipoEmpresa.MEDIO_COMUNICACION);
+      gt.insertarTourEm((long)9632000, "nc8", "lp8", fr8, fs8, 3000, co.getListaTours(), "ne2", true, TipoEmpresa.TECNOLOGIA);
+      gt.insertarTourEm((long)5603267, "nc9", "lp9", fr9, fs9, 3000, co.getListaTours(), "ne3", false, TipoEmpresa.TURISMO);
       
 	}
 }
